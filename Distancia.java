ada7df1dbdb1f6ce67416f7fd56d609c27887f01
janaina <janainarohden@gmail.com>
2022-08-26 15:35:55 -0300

    Algoritmo que mostra a relação entre tempo e velocidade de dois automoveis e proporcionalmente a distancia entre eles.

---
+++ Distancia.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Distancia {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int km = scanner.nextInt();
+        int minutos = km * 2;
+        System.out.println(minutos + " minutos");
+    }
+}
