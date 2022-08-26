ded65593f10ec853df6aeb9d09136681e76f894f
janaina <janainarohden@gmail.com>
2022-08-26 15:31:15 -0300

    Algoritmo com entrada de quatro valores inteiros em seguida calculo da diferença dos produtos.

---
+++ Diferenca.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Diferenca {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        int D = scanner.nextInt();
+        int DIFERENCA = (A * B - C * D);
+        System.out.println("DIFERENCA = " + DIFERENCA);
+    }
+}
